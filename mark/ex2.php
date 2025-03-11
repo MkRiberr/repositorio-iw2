460d7a0b12d93e9797eb3b5e64a11f952f96bc7d
Mark <161876331+MkRiberr@users.noreply.github.com>
2025-03-11 17:29:20 -0300

    Add files via upload

---
+++ mark/ex2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x= 2;
+    $y= 3;
+    $z= 5;
+
+    $media = ($x+$y+$z)/3;
+
+    echo " a media é $media"
+    ?>
+</body>
+</html>
